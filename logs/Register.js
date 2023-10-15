d1348df92898e9ac8dbfef588891ed86bd8eb877
harshu <96584127+iharshuu@users.noreply.github.com>
2023-10-15 12:31:36 +0530

    Add files via upload

---
+++ logs/Register.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import './Register.css';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [file, setFile] = useState(null);
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const register = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('user', user);
+    formData.append('email', email);
+    formData.append('file', file);
+    formData.append('password', password);
+
+    try {
+      const response = await fetch('https://surgeflow1.onrender.com/register', {
+        method: 'POST',
+        body: formData,
+      });
+      if (response.status === 200) {
+        toast.success('Successfully Registered', {
+          position: 'top-right',
+        });
+
+        navigate('/login');
+      }
+      else{
+        toast.error('Something Went Wrong', {
+          position: 'top-right',
+        });
+      }
+    } catch (error) {
+      toast.error(`${error.message}`, {
+        position: 'top-right',
+      });
+    }
+  };
+
+  return (
+    <div className='register'>
+      <div className='hehe1'>
+        <h1 className='app1'>SurgeFlow</h1>
+      </div>
+      <form onSubmit={register} className='register-form'>
+        <div>
+          <label htmlFor='user'>Username:</label>
+          <input id='user' type='text' value={user} onChange={(e) => setUser(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor='email'>Email:</label>
+          <input id='email' type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor='file'>Profile Picture:</label>
+          <input id='file' type='file' onChange={handleFileChange} />
+          <span className='verify'>(It accepts only .jpg, .jpeg, .png)</span>
+        </div>
+        <div>
+          <label htmlFor='password'>Password:</label>
+          <input id='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type='submit'>Register</button>
+      </form>
+      <br />
+      <p className='p1'>
+         Have an account? <Link to='/login'>Log in</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
